743b05b28b5800b1bcc0ea649ea0ba2b933601c6
Arthur Vinicius <thuur.vss@gmail.com>
2023-06-18 21:08:32 -0300

    feat: page in progress

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -37,7 +37,7 @@ export default function Header() {
             Vagas
           </a>
           <a
-            href="/pessoas"
+            href="/devs"
             className="text-xl font-semibold leading-6 text-gray-900"
           >
             Devs
@@ -91,7 +91,7 @@ export default function Header() {
                   Vagas
                 </a>
                 <a
-                  href="/pessoas"
+                  href="/devs"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   Devs
